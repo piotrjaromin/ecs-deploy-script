8797e44a5a25442706a077fc75c3053f22507b1b
Piotr Jaromin <piotr.jaromin@performgroup.com>
2019-04-04 19:36:16 +0200

    added continue deploy and rollback

--- ecs.js
+++ ecs.js
@@ -3,13 +3,17 @@
 'use strict';
 
 const logger = require('simple-node-logger').createSimpleLogger();
-logger.setLevel(process.env.LOG_LEVEL || 'debug');
+logger.setLevel(process.env.LOG_LEVEL || 'error');
 
 const {processCmdAction} = require('./lib/cli/cli');
-const {deploy} = require('./lib/commads/deploy');
+const {deploy} = require('./lib/commands/deploy');
+const {continueDeploy} = require('./lib/commands/continue-deploy');
+const {rollbackDeploy} = require('./lib/commands/rollback-deploy');
 
 const cmdActions = {
     'deploy': deploy,
+    'continueDeploy': continueDeploy,
+    'rollbackDeploy': rollbackDeploy,
 };
 
 processCmdAction(logger, cmdActions);
